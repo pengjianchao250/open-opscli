dfe4b080d8dfcaf5b991c0b4d407888cc5fd8dc1
pengjch <250305917@qq.com>
2026-05-28 19:45:11 +0800

    release: 发布 v0.0.81

--- opscli/version.py
+++ opscli/version.py
@@ -8,7 +8,7 @@ from __future__ import annotations
 from importlib.metadata import PackageNotFoundError, version
 
 PACKAGE_NAME = "aukeys-opscli"
-FALLBACK_VERSION = "0.0.80-dev"
+FALLBACK_VERSION = "0.0.81-dev"
 
 
 def get_version() -> str:
