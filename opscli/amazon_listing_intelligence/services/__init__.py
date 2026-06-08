96af068f9174c4e0b4943d918448703aa1935ce7
huangyongwei <huangyongwei@aukeys.com>
2026-06-08 09:20:42 +0800

    del: 删除测试数据集内容

--- opscli/amazon_listing_intelligence/services/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Amazon Listing Intelligence 服务。"""
-
-from opscli.amazon_listing_intelligence.services.manager import (
-    AmazonListingIntelligenceManager,
-)
-
-__all__ = ["AmazonListingIntelligenceManager"]
